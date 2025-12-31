66813b521b1a5445e086500ced53bd2ff2d599e1
andyshulman8 <andyshulman8@gmail.com>
2025-12-31 10:26:31 -0700

    bug fixing again

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import React, { StrictMode } from 'react'; // Added { StrictMode }
+//import React, { StrictMode } from 'react'; // Added { StrictMode }
 import { createRoot } from 'react-dom/client'; // Added { createRoot }
 import App from './App';
 import './index.css';
+import { StrictMode } from 'react';
 
 // Now you can use them directly
 createRoot(document.getElementById('root')!).render(
