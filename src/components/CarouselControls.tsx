772b29f54edda701bfd79b755a747c4711ac5912
andyshulman8 <andyshulman8@gmail.com>
2026-01-02 22:35:43 -0700

    bug

--- src/components/CarouselControls.tsx
+++ src/components/CarouselControls.tsx
@@ -1,7 +1,2 @@
-import React from 'react';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
-
-interface Props {
-  length: number;
-  export { default } from './CarouselControlsNew';
-  onPrev: () => void;
+// Re-export the new CarouselControls implementation
+export { default } from './CarouselControlsNew';
